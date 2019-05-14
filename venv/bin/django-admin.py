0e1ccb3b35949f7889d900f86b695c6eedb65dc1
ygorpires <ygor_pires@hotmail.com>
2019-05-14 02:12:16 -0300

    teste

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/william/Documentos/WebProjects/DjangoProjects/veproafa/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
